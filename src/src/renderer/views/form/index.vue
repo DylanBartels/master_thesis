e426f850401e855c9088b30c9ef0f7ee311ab04c
Dylan Bartels <d_bartels@live.nl>
2018-05-24 20:22:15 +0200

    Applied datamodel in form for transport request

--- src/src/renderer/views/form/index.vue
+++ src/src/renderer/views/form/index.vue
@@ -1,46 +1,71 @@
 <template>
   <div class="app-container">
     <el-form ref="form" :model="form" label-width="120px">
-      <!--<el-form-item label="Activity name">-->
-        <!--<el-input v-model="form.name"></el-input>-->
-      <!--</el-form-item>-->
       <el-form-item label="Value category">
-        <el-select v-model="form.value_category" placeholder="please select your trustless equivalent">
+        <el-select v-model="form.value_category" placeholder="Select trust equivalent">
           <el-option label="Category 1: €10" value="10"></el-option>
           <el-option label="Category 2: €20" value="20"></el-option>
           <el-option label="Category 3: €50" value="50"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="Activity time">
-        <el-col :span="11">
-          <el-date-picker type="date" placeholder="Pick a date" v-model="form.date1" style="width: 100%;"></el-date-picker>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-time-picker type="fixed-time" placeholder="Pick a time" v-model="form.date2" style="width: 100%;"></el-time-picker>
-        </el-col>
+      <el-collapse v-model="activeName" accordion style="margin-bottom: 22px;">
+        <el-collapse-item title="Pickup" name="1">
+          <el-form-item label="Recipient name">
+            <el-input v-model="form.pickup.name" placeholder="Koninklijke TNT Post BV"></el-input>
+          </el-form-item>
+          <el-form-item label="Street address">
+            <el-input v-model="form.pickup.address" placeholder="Prinses Beatrixlaan 23"></el-input>
+          </el-form-item>
+          <el-form-item label="Postal code">
+            <el-input v-model="form.pickup.postal" placeholder="2595AK"></el-input>
+          </el-form-item>
+          <el-form-item label="City">
+            <el-input v-model="form.pickup.city" placeholder="'S-Gravenhage"></el-input>
+          </el-form-item>
+          <el-form-item label="Country">
+            <el-input v-model="form.pickup.country" placeholder="Netherlands"></el-input>
+          </el-form-item>
+          <el-form-item label="Activity time">
+            <el-col :span="11">
+              <el-date-picker type="date" placeholder="Pick a date" v-model="form.pickup.date_day" style="width: 100%;"></el-date-picker>
+            </el-col>
+            <el-col class="line" :span="2">-</el-col>
+            <el-col :span="11">
+              <el-time-picker type="fixed-time" placeholder="Pick a time" v-model="form.pickup.date_time" style="width: 100%;"></el-time-picker>
+            </el-col>
+          </el-form-item>
+        </el-collapse-item>
+        <el-collapse-item title="Dropoff" name="2">
+          <el-form-item label="Recipient name">
+            <el-input v-model="form.dropoff.name" placeholder="Koninklijke TNT Post BV"></el-input>
+          </el-form-item>
+          <el-form-item label="Street address">
+            <el-input v-model="form.dropoff.address" placeholder="Prinses Beatrixlaan 23"></el-input>
+          </el-form-item>
+          <el-form-item label="Postal code">
+            <el-input v-model="form.dropoff.postal" placeholder="2595AK"></el-input>
+          </el-form-item>
+          <el-form-item label="City">
+            <el-input v-model="form.dropoff.city" placeholder="'S-Gravenhage"></el-input>
+          </el-form-item>
+          <el-form-item label="Country">
+            <el-input v-model="form.dropoff.country" placeholder="Netherlands"></el-input>
+          </el-form-item>
+          <el-form-item label="Activity time">
+            <el-col :span="11">
+              <el-date-picker type="date" placeholder="Pick a date" v-model="form.dropoff.date_day" style="width: 100%;"></el-date-picker>
+            </el-col>
+            <el-col class="line" :span="2">-</el-col>
+            <el-col :span="11">
+              <el-time-picker type="fixed-time" placeholder="Pick a time" v-model="form.dropoff.date_time" style="width: 100%;"></el-time-picker>
+            </el-col>
+          </el-form-item>
+        </el-collapse-item>
+      </el-collapse>
+      <el-form-item label="Dropoff key">
+        <el-input v-model="form.dropoff.public_key" placeholder="Public key"></el-input>
       </el-form-item>
-      <el-form-item label="Instant delivery">
-        <el-switch v-model="form.delivery"></el-switch>
-      </el-form-item>
-      <el-form-item label="Activity type">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="Online activities" name="type"></el-checkbox>
-          <el-checkbox label="Promotion activities" name="type"></el-checkbox>
-          <el-checkbox label="Offline activities" name="type"></el-checkbox>
-          <el-checkbox label="Simple brand exposure" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="Resources">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="Sponsor"></el-radio>
-          <el-radio label="Venue"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <!--<el-form-item label="Activity form">-->
-        <!--<el-input type="textarea" v-model="form.desc"></el-input>-->
-      <!--</el-form-item>-->
-      <el-form-item>
+      <el-form-item style="margin-top: 22px;">
         <el-button type="primary" @click="onSubmit">Create</el-button>
         <el-button @click="onCancel">Cancel</el-button>
       </el-form-item>
@@ -53,38 +78,143 @@
     data () {
       return {
         error: null,
+        activeName: '1',
+        txid: '',
         form: {
           value_category: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          loc: {
-            lat: '',
-            lng: ''
+          pickup: {
+            name: '',
+            address: '',
+            postal: '',
+            city: '',
+            country: '',
+            date_day: '',
+            date_time: ''
+          },
+          dropoff: {
+            name: '',
+            address: '',
+            postal: '',
+            city: '',
+            country: '',
+            date_day: '',
+            date_time: '',
+            public_key: ''
           }
         }
       }
     },
-    created () {
-      this.fetchLoc()
-    },
     methods: {
-      fetchLoc () {
-        let where = require('node-where')
+      // // Possible to do loc instead of address format
+      // fetchLoc () {
+      //   const where = require('node-where')
+      //
+      //   where.is('173.194.33.104', (err, result) => {
+      //     if (err) {
+      //       this.error = err
+      //     } else {
+      //       this.form.loc.lat = result.get('lat')
+      //       this.form.loc.lng = result.get('lng')
+      //     }
+      //   })
+      // },
+      insertForm () {
+        const driver = require('bigchaindb-driver')
 
-        where.is('173.194.33.104', (err, result) => {
-          if (err) {
-            this.error = err
-          } else {
-            this.form.loc.lat = result.get('lat')
-            this.form.loc.lng = result.get('lng')
-          }
+        // Init connection
+        let bdb = new driver.Connection('https://test.bigchaindb.com/api/v1/', {
+          app_id: 'b65e44fe',
+          app_key: 'f5a123dba75e2f17d76a280957a14443'
         })
+
+        // Create a new keypair for Alice and Bob
+        const alice = new driver.Ed25519Keypair()
+        const bob = new driver.Ed25519Keypair()
+
+        console.log('Alice: ', alice.publicKey)
+        console.log('Bob: ', bob.publicKey)
+
+        // Create asset
+        const assetdata = {
+          'value_category': this.form.value_category,
+          'pickup': {
+            'name': this.form.pickup.name,
+            'address': this.form.pickup.address,
+            'postal': this.form.pickup.postal,
+            'city': this.form.pickup.city,
+            'country': this.form.pickup.country,
+            'date_day': this.form.pickup.date_day,
+            'date_time': this.form.pickup.date_time
+          },
+          'dropoff': {
+            'name': this.form.dropoff.name,
+            'address': this.form.dropoff.address,
+            'postal': this.form.dropoff.postal,
+            'city': this.form.dropoff.city,
+            'country': this.form.dropoff.country,
+            'date_day': this.form.dropoff.date_day,
+            'date_time': this.form.dropoff.date_time,
+            'public_key': this.form.dropoff.public_key
+          }
+        }
+        const metadata = {'planet': 'earth'}
+
+        // Creating transaction
+        const txCreateAliceSimple = driver.Transaction.makeCreateTransaction(
+          assetdata,
+          metadata,
+
+          // A transaction needs an output
+          [ driver.Transaction.makeOutput(
+            driver.Transaction.makeEd25519Condition(alice.publicKey))
+          ],
+          alice.publicKey
+        )
+
+        // Signing transaction
+        const txCreateAliceSimpleSigned = driver.Transaction.signTransaction(txCreateAliceSimple, alice.privateKey)
+
+        // Sending
+        const conn = new driver.Connection(bdb)
+        this.txid = txCreateAliceSimpleSigned.id
+
+        conn.postTransactionCommit(txCreateAliceSimpleSigned)
+          .then(retrievedTx => console.log('Transaction', retrievedTx.id, 'successfully posted.'))
+          // With the postTransactionCommit if the response is correct, then the transaction
+          // is valid and commited to a block
+
+          // Transfer bicycle to Bob
+          .then(() => {
+            const txTransferBob = driver.Transaction.makeTransferTransaction(
+              // signedTx to transfer and output index
+              [{ tx: txCreateAliceSimpleSigned, output_index: 0 }],
+              [driver.Transaction.makeOutput(driver.Transaction.makeEd25519Condition(bob.publicKey))],
+              // metadata
+              {price: '100 euro'}
+            )
+
+            // Sign with alice's private key
+            let txTransferBobSigned = driver.Transaction.signTransaction(txTransferBob, alice.privateKey)
+            console.log('Posting signed transaction: ', txTransferBobSigned)
+
+            // Post with commit so transaction is validated and included in a block
+            return conn.postTransactionCommit(txTransferBobSigned)
+          })
+          .then(res => {
+            console.log('Response from BDB server:', res)
+            return res.id
+          })
+          .then(tx => {
+            console.log('Is Bob the owner?', tx['outputs'][0]['public_keys'][0] === bob.publicKey)
+            console.log('Was Alice the previous owner?', tx['inputs'][0]['owners_before'][0] === alice.publicKey)
+          })
+          // Search for asset based on the serial number of the bicycle
+          .then(() => conn.searchAssets('Bicycle Inc.'))
+          .then(assets => console.log('Found assets with serial number Bicycle Inc.:', assets))
       },
       onSubmit () {
-        this.$message('submit!')
+        this.insertForm()
+        this.$message('submitted!')
       },
       onCancel () {
         this.$message({
--- src/src/renderer/views/marketplace/index.vue
+++ src/src/renderer/views/marketplace/index.vue
@@ -1,71 +1,80 @@
 <template>
   <div class="app-container">
-    <el-input placeholder="Filter keyword" v-model="filterText" style="margin-bottom:30px;"></el-input>
-
-    <el-tree class="filter-tree" :data="data2" :props="defaultProps" default-expand-all :filter-node-method="filterNode" ref="tree2"></el-tree>
-
+    <!--<el-table :data="list" v-loading.body="listLoading" element-loading-text="Loading" border fit highlight-current-row>-->
+      <!--<el-table-column align="center" label='ID' width="95">-->
+        <!--<template slot-scope="scope">-->
+          <!--{{scope.$index}}-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+      <!--<el-table-column label="Title">-->
+        <!--<template slot-scope="scope">-->
+          <!--{{scope.row.title}}-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+      <!--<el-table-column label="Author" width="110" align="center">-->
+        <!--<template slot-scope="scope">-->
+          <!--<span>{{scope.row.author}}</span>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+      <!--<el-table-column label="Pageviews" width="110" align="center">-->
+        <!--<template slot-scope="scope">-->
+          <!--{{scope.row.pageviews}}-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+      <!--<el-table-column class-name="status-col" label="Status" width="110" align="center">-->
+        <!--<template slot-scope="scope">-->
+          <!--<el-tag :type="scope.row.status | statusFilter">{{scope.row.status}}</el-tag>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+      <!--<el-table-column align="center" prop="created_at" label="Display_time" width="200">-->
+        <!--<template slot-scope="scope">-->
+          <!--<i class="el-icon-time"></i>-->
+          <!--<span>{{scope.row.display_time}}</span>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
+    <!--</el-table>-->
   </div>
 </template>
 
 <script>
+  // import { getList } from '@/api/table'
+
   export default {
-    watch: {
-      filterText (val) {
-        this.$refs.tree2.filter(val)
-      }
-    },
-
-    methods: {
-      filterNode (value, data) {
-        if (!value) return true
-        return data.label.indexOf(value) !== -1
-      }
-    },
-
     data () {
       return {
-        filterText: '',
-        data2: [{
-          id: 1,
-          label: 'Level one 1',
-          children: [{
-            id: 4,
-            label: 'Level two 1-1',
-            children: [{
-              id: 9,
-              label: 'Level three 1-1-1'
-            }, {
-              id: 10,
-              label: 'Level three 1-1-2'
-            }]
-          }]
-        }, {
-          id: 2,
-          label: 'Level one 2',
-          children: [{
-            id: 5,
-            label: 'Level two 2-1'
-          }, {
-            id: 6,
-            label: 'Level two 2-2'
-          }]
-        }, {
-          id: 3,
-          label: 'Level one 3',
-          children: [{
-            id: 7,
-            label: 'Level two 3-1'
-          }, {
-            id: 8,
-            label: 'Level two 3-2'
-          }]
-        }],
-        defaultProps: {
-          children: 'children',
-          label: 'label'
-        }
+        list: null,
+        listLoading: true
+      }
+    },
+    // filters: {
+    //   statusFilter (status) {
+    //     const statusMap = {
+    //       published: 'success',
+    //       draft: 'gray',
+    //       deleted: 'danger'
+    //     }
+    //     return statusMap[status]
+    //   }
+    // },
+    created () {
+      this.fetchData()
+    },
+    methods: {
+      fetchData () {
+        this.listLoading = true
+        const driver = require('bigchaindb-driver')
+
+        // Init connection
+        let bdb = new driver.Connection('https://test.bigchaindb.com/api/v1/', {
+          app_id: 'b65e44fe',
+          app_key: 'f5a123dba75e2f17d76a280957a14443'
+        })
+
+        const conn = new driver.Connection(bdb)
+        console.log(conn)
+        conn.searchAssets('g')
+          .then(assets => console.log('Found assets with serial number Bicycle Inc.:', assets))
       }
     }
   }
 </script>
-
